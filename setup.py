79e5aa4727dcfd5bb026db87079cdcfa16ba81f2
Greg Back <gback@mitre.org>
2017-04-28 14:28:33 -0500

    Update Trove classifiers.

--- setup.py
+++ setup.py
@@ -50,10 +50,16 @@ setup(
     packages=find_packages(),
     install_requires=install_requires,
     classifiers=[
-        "Programming Language :: Python",
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ]
 )
